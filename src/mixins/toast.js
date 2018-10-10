77b35e38bbcbf886c531fc54bb6f98f1bb412b52
renshuhao <rensh@wealthlake.com.cn>
2018-10-11 00:19:08 +0800

    Initial commit

---
+++ src/mixins/toast.js
@@ -0,0 +1,24 @@
+import wepy from 'wepy'
+
+export default class wepyToast extends wepy.mixin {
+  onLoad () {  // onLoad生命周期钩子函数
+    this.showToast()
+  }
+  noMore () {  // 普通方法直接定义到class的静态方法
+    wepy.showToast({  // wepy.showToast 等同于 wx.showToast
+      title: '没有更多了...',
+      icon: 'none',
+      duration: 1500
+    })
+  }
+  showToast () {
+    wepy.showToast({
+      title: '拼命加载中...',
+      icon: 'loading',
+      duration: 3000
+    })
+  }
+  hideToast () {
+    wepy.hideToast()
+  }
+}
